2fb36e747b759b127b268aa42c0db8cda729afbc
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2023-06-15 13:10:37 +0300

    feat: simple two node processor

--- src/main/java/myapps/Pipe.java
+++ src/main/java/myapps/Pipe.java
@@ -5,13 +5,12 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.KStream;
 
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 public class Pipe {
     public static void main(String[] args)  throws Exception{
-
 
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
@@ -27,5 +26,22 @@ public class Pipe {
 
         final KafkaStreams streams = new KafkaStreams(topology, props);
 
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
     }
 }
