4149f4fb3e24f9e7bc7f37c290a966749c8ce357
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2023-06-17 22:37:36 +0300

    feat: WordCount define source

--- src/main/java/myapps/WordCount.java
+++ src/main/java/myapps/WordCount.java
@@ -1,7 +1,9 @@
 package myapps;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
 
 import java.util.Properties;
 
@@ -12,6 +14,10 @@ public class WordCount {
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> source = builder.stream("streams-plain-text");
+
 
     }
 }
